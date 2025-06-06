3e967f0d599c4ac0ea4996e08c63e504037751be
DESKTOP-8OJB9DC\Admin <nyb1711@gmail.com>
2025-06-06 22:55:55 +0700

    fix: fix fcm worker

--- frontend/app/src/main/java/com/trio/stride/domain/usecase/fcmnotification/RefreshAndSaveFCMTokenUseCase.kt
+++ frontend/app/src/main/java/com/trio/stride/domain/usecase/fcmnotification/RefreshAndSaveFCMTokenUseCase.kt
@@ -3,9 +3,11 @@ package com.trio.stride.domain.usecase.fcmnotification
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessaging
 import com.trio.stride.data.datastoremanager.FCMTokenManager
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.lastOrNull
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RefreshAndSaveFCMTokenUseCase @Inject constructor(
@@ -14,13 +16,15 @@ class RefreshAndSaveFCMTokenUseCase @Inject constructor(
     private val deleteFCMTokenUseCase: DeleteFCMTokenUseCase
 ) {
     suspend operator fun invoke() {
-        val newToken = FirebaseMessaging.getInstance().token.await()
-        val oldToken = fcmTokenManager.getToken().firstOrNull()
-        if (!oldToken.isNullOrBlank() && oldToken != newToken) {
-            deleteFCMTokenUseCase(oldToken)
+        withContext(Dispatchers.IO) {
+            val newToken = FirebaseMessaging.getInstance().token.await()
+            val oldToken = fcmTokenManager.getToken().firstOrNull()
+            if (!oldToken.isNullOrBlank() && oldToken != newToken) {
+                deleteFCMTokenUseCase(oldToken)
+            }
+            saveFCMTokenUseCase(newToken).lastOrNull()
+            fcmTokenManager.setToken(newToken)
+            Log.i("SEND_FCM_TOKEN_TO_SERVER_AND_SAVE", newToken)
         }
-        saveFCMTokenUseCase(newToken).lastOrNull()
-        fcmTokenManager.setToken(newToken)
-        Log.i("SEND_FCM_TOKEN_TO_SERVER_AND_SAVE", newToken)
     }
 }
